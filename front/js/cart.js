82980eb511ecc68cf927b90b994fd978d7cd5495
Natsil5 <livernoistristan@gmail.com>
2023-03-17 14:47:42 +0100

    panier v2

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,7 @@
 
 // Récupérer le panier depuis le local storage
 let panier = JSON.parse(localStorage.getItem("panier"));
+console.table(panier)
 
 // Sélectionner l'élément HTML où afficher le contenu du panier
 let affichage = document.querySelector("#cart__items");
@@ -11,22 +12,28 @@ if (panier.length === 0) {
   affichage.innertext = "<p>Votre panier est vide.</p>";
 } else {
   // Générer le HTML pour chaque produit dans le panier
-  let html = "";
-  for (let produit of panier) {
+  for (let article of panier) {
+    let html = "";
+    console.log(article.produit)
+    fetch(`http://localhost:3000/api/products/${article.produit}`)
+     .then((res) => res.json())
+     .then((obj) => {
+        console.log(obj)
     html += `
-      <div class="cart__item" data-produit="${produit.produit}" data-couleur="${produit.couleur}">
+      <div class="cart__item" data-produit="${article.produit}" data-couleur="${article.couleur}">
         <div class="cart__item__img">
-          <img src="${produit.imageUrl}" alt="${produit.altTxt}">
+          <img src="${obj.imageUrl}" alt="${obj.altTxt}">
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__titlePrice">
-            <h2>${produit.name}</h2>
-            <p>${produit.price}€</p>
+            <h2>${obj.name}</h2>
+            <p>${article.couleur}<p>
+            <p>${obj.price}€</p>
           </div>
           <div class="cart__item__content__settings">
             <div class="cart__item__content__settings__quantity">
               <p>Quantité :</p>
-              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${produit.quantite}">
+              <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${article.quantite}">
             </div>
             <div class="cart__item__content__settings__delete">
               <p class="deleteItem">Supprimer</p>
@@ -35,7 +42,17 @@ if (panier.length === 0) {
         </div>
       </div>
     `;
+     
+      // Ajouter le HTML au DOM
+    affichage.innerHTML += html;
+    
+    //listener
+    })
   }
+}
 
-  // Ajouter le HTML au DOM
-  affichage.innerHTML = html;}
+//modifier les quantités
+
+//supprimer article
+
+//afficher total du prix et nombre d'article
